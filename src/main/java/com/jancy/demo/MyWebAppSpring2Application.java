d778fbb3dd5ae7dfb9dbfa5481004a192372ab05
Jancy <s.jancyjerin@gmail.com>
2023-07-06 00:05:40 -0500

    file modified

---
+++ src/main/java/com/jancy/demo/MyWebAppSpring2Application.java
@@ -0,0 +1,13 @@
+package com.jancy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebAppSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebAppSpring2Application.class, args);
+	}
+
+}
